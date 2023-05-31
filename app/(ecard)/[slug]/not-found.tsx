2491568430bf51f89c1592f8befd5ea34354d333
Eduardo Hidalgo <eduardohidalgo.dev@gmail.com>
2023-05-31 15:41:05 -0500

    Improved not-found page, now works as all not-found pages

--- app/(ecard)/[slug]/not-found.tsx
+++ app/(ecard)/[slug]/not-found.tsx
@@ -9,18 +9,18 @@ export default function NotFound() {
   }, []);
 
   return (
-    <main className="grid min-h-full bg-white py-24">
-      <p className="font-extrabold text-7xl bg-primary-500 w-full text-center text-white py-8">
-        404
-      </p>
+    <main className="flex flex-col bg-white pt-24">
+      <div className="flex flex-col items-center justify-center font-extrabold text-7xl bg-primary-500 w-full text-white">
+        <p className="py-16">404</p>
+      </div>
       <div className="text-center w-full max-w-3xl px-6 lg:px-8 py-8 m-auto">
         <h1 className="font-title mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          ¡Ups! No se encontró el eCard que buscas
+          ¡Ups! No se encontró la página que buscas
         </h1>
         <p className="font-sans mt-6 text-base leading-7 text-gray-600">
-          Lo sentimos, la página de eCard que has intentado ver no existe o ya
-          no está disponible. Si el problema persiste, contacta con nuestro
-          equipo de soporte.
+          Lo sentimos, la página que has intentado ver no existe o ya no está
+          disponible. Si el problema persiste, contacta con nuestro equipo de
+          soporte.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <ButtonLink href="/" label="Volver al inicio" />
